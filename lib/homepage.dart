165fba7ab4fde3e527af4703173af54d7d12492a
MIGHTY1o1 <shubhagarwalcse@gmail.com>
2023-03-30 22:01:51 +0530

    changes added

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -2,6 +2,7 @@ import 'package:cardsaver/constants.dart';
 import 'package:cardsaver/notesave/noteScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:hive/hive.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,12 +11,12 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'NoteSaver',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Card Saver'),
     );
   }
 }
@@ -49,121 +50,190 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
         appBar:  AppBar(
 
-          backgroundColor: Colors.deepPurple,
+          backgroundColor: Colors.deepPurpleAccent,
           leading: IconButton(
             icon: Icon(Icons.menu),
             tooltip: 'Menu',
             onPressed: () {},
           ), //IconButton
           actions: <Widget>[
-            //IconButton
-            IconButton(
-              icon: Icon(Icons.person),
-              tooltip: 'Person',
-              onPressed: () {},
-            ), //IconButton
+            CircleAvatar(backgroundImage: AssetImage("assets/images/1.png"),),
           ], //<Widget>[]
         ),
-      body: CustomScrollView(
-        slivers: [
 
-      SliverList(
-      delegate: SliverChildListDelegate(
-      [
-        Expanded(child: SearchField()),
-        // Container(
-        //   width: double.infinity,
-        //   height: 50,
-        //   color: Colors.deepPurple,
-        //
-        //   child: Center(
-        //     child: Text(
-        //       'Header',
-        //       style: TextStyle(color: Colors.white, letterSpacing: 4),
-        //     ),
-        //   ),
-        // ),
-        // ListView(
-        //   scrollDirection: Axis.horizontal,
-        //   children: <Widget>[
-        //     Container(
-        //       width: 150.0,
-        //       color: Colors.red,
-        //     ),
-        //     Container(
-        //       width: 150.0,
-        //       color: Colors.blue,
-        //     ),
-        //     Container(
-        //       width: 150.0,
-        //       color: Colors.green,
-        //     ),
-        //   ],
-        // ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.deepPurpleAccent
+        ),
+        child: CustomScrollView(
+          slivers:
+          [SliverList
+            (delegate:
+              SliverChildListDelegate(
+          [
+          Expanded(child:SearchField()), SizedBox(height: 20,),
+          hScroll(), SizedBox(height: 20,),
+          Align(
+           alignment: Alignment.bottomRight,
+           child:ElevatedButton.icon(
+             style: ButtonStyle(
+               backgroundColor: MaterialStateProperty.all(Colors.transparent),
+             ),
+             onPressed: () {},
+             icon: Icon( // <-- Icon
+               Icons.sort_rounded,
+               size: 14.0,
+               color: Colors.blue,
+             ),
+             label: Text('Sortby'), // <-- Text
+           ),
+         ),
 
-        ListView.builder(
-          shrinkWrap: true,
-          itemCount: 5,
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              title: Container(
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child:
-                  Row(
-                    children: [
-                      Image.asset(
-                        'assets/images/devs.jpg',
-                        width: 50,
-                        height: 50,
-                      )
-                    ,
-                      Spacer(),
-                      Column(
-                        children: const [
-                          Text('Google', style: TextStyle(color: Colors.black54)),
-                          Text('com.google.package', style: TextStyle(color: Colors.black54)),
-                        ],
+          SizedBox(height: 20,),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: languages.length,
+            itemBuilder: (BuildContext context,  index) {
+              return Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
                       ),
-                      Spacer(),
-                      Image.asset(
-                        'assets/images/devs.jpg',
-                        width: 50,
-                        height: 50,
-                      )
-                    ],
-                  )
+                margin: EdgeInsets.only(left: 20, right: 20 ,top: 10,bottom: 10),
+                child:  ListTile(
+                    contentPadding: EdgeInsets.symmetric(horizontal: 20.0),
+                    shape: RoundedRectangleBorder( //<-- SEE HERE
+                      side: BorderSide(width: 2),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    title: Text('${languages[index].name}',style: TextStyle(color: Colors.black),),
+
+                    subtitle: Text(".compackage",style: TextStyle(color: Colors.black),),
+
+                    leading: CircleAvatar(backgroundImage: AssetImage("assets/images/$index.png")),
+                    trailing: Icon(Icons.copy_all,color: Colors.black,),
+                    onTap: (){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => FirstRoute()),
+                      );
+                    },
 
 
-              ),
-            );
-          },
-        ),
-        IconButton(
-          icon: const Icon(Icons.add),
-          iconSize: 50,
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => FirstRoute()),
-            );
-          },
-        ),
-      ],
+                  ),
+              );
+            },
 
-    ),
-      ),
+          ),
+
+
+ //add button
+          Align(alignment: Alignment.bottomRight,
+         child: IconButton(
+           padding: EdgeInsets.all(10),
+            icon: const Icon(Icons.add),
+            iconSize: 50,
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => FirstRoute()),
+              );
+            },
+          ),
+          ),
+
 
         ],
-
+    ),
+        ),
+          ],
+        ),
       ),
     );
   }
 }
 
+
+
+//scroll view
+class hScroll extends StatefulWidget {
+  const hScroll({Key? key}) : super(key: key);
+
+  @override
+  State<hScroll> createState() => _hScrollState();
+}
+
+class _hScrollState extends State<hScroll> {
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      height: 50,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child:InkWell(
+                onTap: (){},
+                child: Container(
+                  width: 100,
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.white),
+                  child:Center( child: Text("All category",style: TextStyle(fontSize: 10,color: Colors.black),),),
+                )
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child:InkWell(
+                onTap: (){},
+                child: Container(
+                  width: 100,
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.white),
+                  child:Center( child: Text("Passwords",style: TextStyle(fontSize: 10,color: Colors.black),),),
+                )
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child:InkWell(
+                onTap: (){},
+                child: Container(
+                  width: 100,
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.white),
+                  child:Center( child: Text("Personal Information",style: TextStyle(fontSize: 10,color: Colors.black),),),
+                )
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child:InkWell(
+                onTap: (){},
+                child: Container(
+                  width: 100,
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.white),
+                  child:Center( child: Text("Emails",style: TextStyle(fontSize: 10,color: Colors.black),),),
+                )
+            ),
+          ),Padding(
+            padding: const EdgeInsets.all(8.0),
+            child:InkWell(
+                onTap: (){},
+                child: Container(
+                  width: 100,
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.white),
+                  child:Center( child: Text("Addresses",style: TextStyle(fontSize: 10,color: Colors.black),),),
+                )
+            ),
+          ),
+
+
+        ],
+      ),
+    );
+  }
+}
+
+//search field
 class SearchField extends StatelessWidget {
   const SearchField({
     Key? key,
@@ -174,7 +244,7 @@ class SearchField extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(left: 20,right: 20,top: 20),
       child: TextFormField(
-        autofocus: false,
+        autofocus: true,
         style: const TextStyle(color: Colors.black),
         decoration: InputDecoration(
           hintText: "Search Passwords",
@@ -192,26 +262,7 @@ class SearchField extends StatelessWidget {
   }
 }
 
-Widget topBar() {
-  return AppBar(
-
-    backgroundColor: Colors.deepPurple,
-    leading: IconButton(
-      icon: Icon(Icons.menu),
-      tooltip: 'Menu',
-      onPressed: () {},
-    ), //IconButton
-    actions: <Widget>[
-      //IconButton
-      IconButton(
-        icon: Icon(Icons.person),
-        tooltip: 'Person',
-        onPressed: () {},
-      ), //IconButton
-    ], //<Widget>[]
-  );
-}
-
+//navigation
 class FirstRoute extends StatelessWidget {
   const FirstRoute({super.key});
 
@@ -236,3 +287,14 @@ class FirstRoute extends StatelessWidget {
     );
   }
 }
+
+//Global declarations
+class Language {
+  String name;
+  bool isChecked;
+  Language(this.name, {this.isChecked = false});
+
+}
+
+final List<Language> languages = [Language('Twiiter') ,Language('Google'),  Language('instagram') , Language('Github'),Language('Slack'),Language('Linkedin')];
+
