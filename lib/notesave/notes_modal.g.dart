fa73422c5375bb80138e8a9acc1a3abf6f68f129
Akshay <Akshaygarg247@gmail.com>
2023-04-02 12:53:44 +0530

    added social medi category wise

--- lib/notesave/notes_modal.g.dart
+++ lib/notesave/notes_modal.g.dart
@@ -48,3 +48,77 @@ class NotesModalAdapter extends TypeAdapter<NotesModal> {
           runtimeType == other.runtimeType &&
           typeId == other.typeId;
 }
+
+class SocialModalAdapter extends TypeAdapter<SocialModal> {
+  @override
+  final int typeId = 1;
+
+  @override
+  SocialModal read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return SocialModal(
+      username: fields[0] as String,
+      password: fields[1] as String,
+      category: fields[2] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, SocialModal obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.username)
+      ..writeByte(1)
+      ..write(obj.password)
+      ..writeByte(2)
+      ..write(obj.category);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is SocialModalAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class CategoryModalAdapter extends TypeAdapter<CategoryModal> {
+  @override
+  final int typeId = 2;
+
+  @override
+  CategoryModal read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return CategoryModal(
+      category: fields[0] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, CategoryModal obj) {
+    writer
+      ..writeByte(1)
+      ..writeByte(0)
+      ..write(obj.category);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is CategoryModalAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
